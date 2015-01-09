392b32e7d74686a86a8882c75c84f2097d9cc275
Joshua Heimbach <joshkh@gmail.com>
2015-01-09 18:08:33 +0000

    Added test for password recovery.

---
+++ testmodel/webapp/selenium/test/email-forgot-password-test.py
@@ -0,0 +1,91 @@
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from test.testmodeltestcase import TestModelTestCase as Super
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from imguerillauser import IMGuerillaUser
+import xml.etree.ElementTree as ET
+import unittest, re, time
+
+class EmailForgotPasswordTest(Super):
+
+    def setUp(self):
+        Super.setUp(self)
+        self.im_guerilla_user = IMGuerillaUser("password");
+        self.im_guerilla_user.create()
+
+    def test_email_forgot_link(self):
+        # Create a second web driver for the sharee account
+        browser = self.browser
+
+        # Confirm that we can log in
+        browser.get(self.base_url + "/login.do")
+        browser.find_element_by_name("username").clear()
+        browser.find_element_by_name("username").send_keys(self.im_guerilla_user.name)
+        browser.find_element_by_name("password").clear()
+        browser.find_element_by_name("password").send_keys(self.im_guerilla_user.password)
+        browser.find_element_by_name("action").click()
+        welcome_message = self.wait_for_elem("#msg")
+        self.assertIn('Logged in as user "' + self.im_guerilla_user.name + '"', welcome_message.text)
+
+        # Log out and then request password reset email
+        browser.get(self.base_url + "/logout.do")
+        browser.get(self.base_url + "/login.do")
+
+        # Make sure that the password request form is hidden
+        wait = WebDriverWait(browser, 10)
+
+        self.wait_for_elem("#passwordDiv")
+        password_reset_div = wait.until(EC.invisibility_of_element_located((By.ID,'passwordDiv')))
+
+        # Toggle the password reset form
+        forgot_password_link = browser.find_element_by_xpath('//div[@id="login"]//a').click()
+
+        # Make sure that the password request form is shown
+        password_reset_div = wait.until(EC.visibility_of_element_located((By.ID,'passwordDiv')))
+
+        # Enter our email address into the password reset form
+        username_input = browser.find_element_by_xpath('//form[@id="requestPasswordForm"]//input[@name="username"]')
+        username_input.send_keys(self.im_guerilla_user.name)
+        request_password_button = browser.find_element_by_xpath('//form[@id="requestPasswordForm"]//input[@name="action"]').click()
+
+        time.sleep(10)
+
+        # Get out user's latest email.
+        last_mail = self.im_guerilla_user.get_last_mail()
+        mail_body = last_mail["mail_body"].decode('string_escape')
+
+        # Parse our password reset link
+        root = ET.fromstring(mail_body)
+        href = root.find(".//a").get("href")
+
+        # Open our password reset link
+        browser.get(self.base_url)
+        browser.get(href)
+
+        new_password = "newpassword"
+
+        # Enter our new password
+        browser.find_element_by_xpath('//input[@name="newpassword"]').send_keys(new_password)
+        browser.find_element_by_xpath('//input[@name="newpassword2"]').send_keys(new_password)
+        browser.find_element_by_xpath('//input[@value="Change password"]').click()
+
+        # Confirm password was changed
+        password_changed_message = self.wait_for_elem("#msg")
+        self.assertIn("Your password was changed successfully", password_changed_message.text)
+        self.im_guerilla_user.set_password(new_password)
+        
+        # Log out / Log in using new password
+        browser.get(self.base_url + "/logout.do")
+        browser.get(self.base_url + "/login.do")
+        browser.find_element_by_name("username").clear()
+        browser.find_element_by_name("username").send_keys(self.im_guerilla_user.name)
+        browser.find_element_by_name("password").clear()
+        browser.find_element_by_name("password").send_keys(self.im_guerilla_user.password)
+        browser.find_element_by_name("action").click()
+        welcome_message = self.wait_for_elem("#msg")
+        self.assertIn('Logged in as user "' + self.im_guerilla_user.name + '"', welcome_message.text)
+        
+    def tearDown(self):
+        self.im_guerilla_user.delete()
---
+++ testmodel/webapp/selenium/test/imguerillauser.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import os
+import uuid
+import random
+import time
+import string
+from nose.plugins.skip import Skip, SkipTest
+
+DEFAULT_BASE = 'http://localhost:8080/intermine-demo'
+
+class IMGuerillaUser:
+    """A helper class for creating new users on a target webservice"""
+
+    def __init__(self, password=None):
+        self.base_url = os.getenv('TESTMODEL_BASE', DEFAULT_BASE)
+        self.password = password if password is not None else 'password'
+
+    def create(self):
+        """Create the new user - SHOULD BE CALLED IN SETUP"""
+
+        try:
+            self.delete()
+        except:
+            pass
+
+        self.name = "intermine-test"
+        epoch_time = str(int(time.time()))
+        rhash = ''.join(random.choice(string.digits) for _ in range(8))
+        self.name += epoch_time
+        self.name += rhash
+
+        url = "http://api.guerrillamail.com/ajax.php?f=set_email_user&email_users=" + self.name
+        guerilla_result = requests.get(url)
+        
+        try:
+            json = guerilla_result.json()
+        except:
+            raise SkipTest("Guerilla Mail Not working")
+
+        self.guerilla = json
+
+        self.name = self.guerilla["email_addr"]
+
+        methodURI = '/service/users'
+        payload = {'name': self.name, 'password': self.password}
+
+        result = requests.post(self.base_url + methodURI, data=payload)
+        j = result.json()
+
+
+        if j['statusCode'] == 200:
+            print "User created successfully: " + self.name
+        else:
+            print "Error creating user: " + j['error']
+
+    def _get_deregistration_token(self):
+        """Get the deregistration token for this user"""
+        methodURI = '/service/user/deregistration'
+        result = requests.post(self.base_url + methodURI, auth=(self.name, self.password))
+
+        j = result.json()
+        return j['token']['uuid']
+
+    def delete(self):
+        print "deleting with password", self.password
+        """Delete this user from the webservice - YOU MUST CALL THIS IN tearDown!!"""
+        methodURI = '/service/user'
+        payload = {'deregistrationToken': self._get_deregistration_token()}
+        requests.delete(self.base_url + methodURI, params=payload, auth=(self.name, self.password))
+
+        url = "http://api.guerrillamail.com/ajax.php?f=forget_me&sid_token=" + self.guerilla["sid_token"]
+        return requests.get(url).json()
+
+    def list_mail(self):
+        url = "http://api.guerrillamail.com/ajax.php?f=get_email_list&offset=0&sid_token=" + self.guerilla["sid_token"]
+        return requests.get(url).json()
+
+    def get_last_mail(self):
+        response = self.list_mail()
+        mail_id = response["list"][0]["mail_id"]
+        url = "http://api.guerrillamail.com/ajax.php?f=fetch_email&email_id=" + str(mail_id) + "&sid_token=" + str(self.guerilla["sid_token"])
+        return requests.get(url).json()
+
+    def set_password(self, password):
+        self.password = password
